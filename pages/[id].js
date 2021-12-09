d821a7270d013fa30e95ae26e219c51795f869af
Talles Corradini <tallescorradini@gmail.com>
2021-12-09 09:46:53 -0300

    feat: add premium content from authenticated users

--- pages/[id].js
+++ pages/[id].js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import Video from "react-player";
+
 import { supabase } from "../utils/supabase";
 
 const LessonsDetails = ({ lesson }) => {
+  const [videoUrl, setVideoUrl] = useState();
+
+  const getPremiumContent = async () => {
+    const { data } = await supabase
+      .from("premium_content")
+      .select("video_url")
+      .eq("id", lesson.id)
+      .single();
+
+    setVideoUrl(data?.video_url);
+  };
+
+  useEffect(() => {
+    getPremiumContent();
+  }, []);
+
   return (
     <div className="w-full max-w-3xl mx-auto py-16 px-8">
       <h1 className="text-3xl mb-6">{lesson.title}</h1>
       <p>{lesson.description}</p>
+      {!!videoUrl ? <Video url={videoUrl} width="100%" /> : null}
     </div>
   );
 };
